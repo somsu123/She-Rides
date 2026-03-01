615fadeb67ca7c4d02151c6c0bc944e6eb442d84
Somsubhro karmakar <somsubhrakarmakar2341@gmail.com>
2026-03-01 19:12:00 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     hmr: {
       overlay: false,
     },
-    allowedHosts: ["she-rides.onrender.com"],
+    allowedHosts: ["https://she-rides-1.onrender.com/"],
   },
   plugins: [react()],
   resolve: {
